60be207762b23220514e150e007105f835599b14
CommanderBeelo <xxxbilliamxxx@gmail.com>
2023-06-01 21:40:00 -0400

    Updated Vanilla Damage PBO
    Sourced from the BBP Discord

--- BBP_Vanilla_Damage/config.cpp
+++ BBP_Vanilla_Damage/config.cpp
@@ -215,7 +215,7 @@ class CfgVehicles
 				{
 					class Health
 					{
-						hitpoints=25000;//ONLY EDIT ME
+						hitpoints=30000;//ONLY EDIT ME
 						transferToGlobalCoef=0;
 						healthLevels[]=
 						{
@@ -319,7 +319,7 @@ class CfgVehicles
 				{
 					class Health
 					{
-						hitpoints=75000;//ONLY EDIT ME
+						hitpoints=50000;//ONLY EDIT ME
 						transferToGlobalCoef=0;
 						healthLevels[]=
 						{
@@ -438,7 +438,7 @@ class CfgVehicles
 				{
 					componentNames[]=
 					{
-						"t2_metal"
+						"t3_wall_concrete_bricks"
 					};
 				};
 				class t2_metal_2: BBP_Tier_2
@@ -452,7 +452,13 @@ class CfgVehicles
 				{
 					componentNames[]=
 					{
-						"t1_wall_logs_horizontal",
+						"t1_wall_logs_horizontal"
+					};
+				};
+				class t1_wall_logs_horizontal_2: BBP_Tier_1
+				{
+					componentNames[]=
+					{
 						"t1_wall_logs_horizontal_2"
 					};
 				};
@@ -552,8 +558,883 @@ class CfgVehicles
 	
 	class BBP_GATE_BASE: BBP_WALL_BASE
 	{
+		class DamageSystem
+		{
+			class GlobalHealth
+			{
+				class Health
+				{
+					hitpoints=100;
+				};
+			};
+			class GlobalArmor
+			{
+				class Projectile
+				{
+					class Health
+					{
+						damage=0;
+					};
+					class Blood
+					{
+						damage=0;
+					};
+					class Shock
+					{
+						damage=0;
+					};
+				};
+				class FragGrenade
+				{
+					class Health
+					{
+						damage=0;
+					};
+					class Blood
+					{
+						damage=0;
+					};
+					class Shock
+					{
+						damage=0;
+					};
+				};
+			};
+			class DamageZones
+			{
+				class BBP_Tier_1
+				{
+					class Health
+					{
+						hitpoints=21000; //ONLY EDIT ME
+						transferToGlobalCoef=0;
+						healthLevels[]=
+						{
+							
+							{
+								1,
+								
+								{
+									"DZ\gear\camping\data\fence_pile_of_planks.rvmat"
+								}
+							},
+							
+							{
+								0.69999999,
+								
+								{
+									"DZ\gear\camping\data\fence_pile_of_planks_worn.rvmat"
+								}
+							},
+							
+							{
+								0.5,
+								
+								{
+									"DZ\gear\camping\data\fence_pile_of_planks_damage.rvmat"
+								}
+							},
+							
+							{
+								0.30000001,
+								
+								{
+									"DZ\gear\camping\data\fence_pile_of_planks_bdamage.rvmat"
+								}
+							},
+							
+							{
+								0,
+								
+								{
+									"DZ\gear\camping\data\fence_pile_of_planks_bdamage.rvmat"
+								}
+							}
+						};
+					};
+					class ArmorType
+					{
+						class Projectile
+						{
+							class Health
+							{
+								damage=1;
+							};
+							class Blood
+							{
+								damage=0;
+							};
+							class Shock
+							{
+								damage=0;
+							};
+						};
+						class Melee
+						{
+							class Health
+							{
+								damage=0.64999998;
+							};
+							class Blood
+							{
+								damage=0;
+							};
+							class Shock
+							{
+								damage=0;
+							};
+						};
+						class FragGrenade
+						{
+							class Health
+							{
+								damage=50;
+							};
+							class Blood
+							{
+								damage=0;
+							};
+							class Shock
+							{
+								damage=0;
+							};
+						};
+					};
+					componentNames[]=
+					{
+						"T1"
+					};
+					fatalInjuryCoef=-1;
+				};
+				class BBP_Tier_2
+				{
+					class Health
+					{
+						hitpoints=30000;//ONLY EDIT ME
+						transferToGlobalCoef=0;
+						healthLevels[]=
+						{
+							
+							{
+								1,
+								
+								{
+									"DZ\gear\camping\data\fence_pile_of_planks.rvmat"
+								}
+							},
+							
+							{
+								0.69999999,
+								
+								{
+									"DZ\gear\camping\data\fence_pile_of_planks_worn.rvmat"
+								}
+							},
+							
+							{
+								0.5,
+								
+								{
+									"DZ\gear\camping\data\fence_pile_of_planks_damage.rvmat"
+								}
+							},
+							
+							{
+								0.30000001,
+								
+								{
+									"DZ\gear\camping\data\fence_pile_of_planks_bdamage.rvmat"
+								}
+							},
+							
+							{
+								0,
+								
+								{
+									"DZ\gear\camping\data\fence_pile_of_planks_bdamage.rvmat"
+								}
+							}
+						};
+					};
+					class ArmorType
+					{
+						class Projectile
+						{
+							class Health
+							{
+								damage=1;
+							};
+							class Blood
+							{
+								damage=0;
+							};
+							class Shock
+							{
+								damage=0;
+							};
+						};
+						class Melee
+						{
+							class Health
+							{
+								damage=0.64999998;
+							};
+							class Blood
+							{
+								damage=0;
+							};
+							class Shock
+							{
+								damage=0;
+							};
+						};
+						class FragGrenade
+						{
+							class Health
+							{
+								damage=50;
+							};
+							class Blood
+							{
+								damage=0;
+							};
+							class Shock
+							{
+								damage=0;
+							};
+						};
+					};
+					componentNames[]=
+					{
+						"T2"
+					};
+					fatalInjuryCoef=-1;
+				};
+				class BBP_Tier_3
+				{
+					class Health
+					{
+						hitpoints=50000;//ONLY EDIT ME
+						transferToGlobalCoef=0;
+						healthLevels[]=
+						{
+							
+							{
+								1,
+								
+								{
+									"DZ\gear\camping\data\fence_pile_of_planks.rvmat"
+								}
+							},
+							
+							{
+								0.69999999,
+								
+								{
+									"DZ\gear\camping\data\fence_pile_of_planks_worn.rvmat"
+								}
+							},
+							
+							{
+								0.5,
+								
+								{
+									"DZ\gear\camping\data\fence_pile_of_planks_damage.rvmat"
+								}
+							},
+							
+							{
+								0.30000001,
+								
+								{
+									"DZ\gear\camping\data\fence_pile_of_planks_bdamage.rvmat"
+								}
+							},
+							
+							{
+								0,
+								
+								{
+									"DZ\gear\camping\data\fence_pile_of_planks_bdamage.rvmat"
+								}
+							}
+						};
+					};
+					class ArmorType
+					{
+						class Projectile
+						{
+							class Health
+							{
+								damage=1;
+							};
+							class Blood
+							{
+								damage=0;
+							};
+							class Shock
+							{
+								damage=0;
+							};
+						};
+						class Melee
+						{
+							class Health
+							{
+								damage=0.64999998;
+							};
+							class Blood
+							{
+								damage=0;
+							};
+							class Shock
+							{
+								damage=0;
+							};
+						};
+						class FragGrenade
+						{
+							class Health
+							{
+								damage=50;
+							};
+							class Blood
+							{
+								damage=0;
+							};
+							class Shock
+							{
+								damage=0;
+							};
+						};
+					};
+					componentNames[]=
+					{
+						"T3"
+					};
+					fatalInjuryCoef=-1;
+				};
+				
+				//WALL, GATE, ROOF FLOOR RAID
+				class t3_wall_concrete_bricks_2: BBP_Tier_3
+				{
+					componentNames[]=
+					{
+						"t3_wall_concrete_bricks_2"
+					};
+				};
+				class t3_wall_concrete_bricks: BBP_Tier_3
+				{
+					componentNames[]=
+					{
+						"t3_wall_concrete_bricks"
+					};
+				};
+				class t2_metal: BBP_Tier_2
+				{
+					componentNames[]=
+					{
+						"t2_metal"
+					};
+				};
+				class t2_metal_2: BBP_Tier_2
+				{
+					componentNames[]=
+					{
+						"t2_metal_2"
+					};
+				};
+				class t1_wall_logs_horizontal: BBP_Tier_1
+				{
+					componentNames[]=
+					{
+						"t1_wall_logs_horizontal"
+					};
+				};
+				class t1_wall_logs_horizontal_2: BBP_Tier_1
+				{
+					componentNames[]=
+					{
+						"t1_wall_logs_horizontal_2"
+					};
+				};
+				class t1_wall_planks: BBP_Tier_1
+				{
+					componentNames[]=
+					{
+						"t1_wall_planks"
+					};
+				};
+				//DOOR RAIDING
+				class t3_door: BBP_Tier_3
+				{
+					componentNames[]=
+					{
+						"t3_door"
+					};
+				};
+				class t2_door: BBP_Tier_2
+				{
+					componentNames[]=
+					{
+						"t2_door"
+					};
+				};
+				class t1_door: BBP_Tier_1
+				{
+					componentNames[]=
+					{
+						"t1_door"
+					};
+				};
+				class window_bars: BBP_Tier_1
+				{
+					componentNames[]=
+					{
+						"window_bars"
+					};
+				};
+				class Wall_Barbedwire_1: BBP_Tier_1
+				{
+					componentNames[]=
+					{
+						"wall_barbedwire_1",
+						"wall_barbedwire_1_mounted"
+					};
+					inventorySlots[]=
+					{
+						"Wall_Barbedwire_1"
+					};
+					fatalInjuryCoef=-1;
+				};
+				class Wall_Barbedwire_2: BBP_Tier_1
+				{
+					componentNames[]=
+					{
+						"wall_barbedwire_2",
+						"wall_barbedwire_2_mounted"
+					};
+					inventorySlots[]=
+					{
+						"Wall_Barbedwire_2"
+					};
+				};
+			};
+		};
 	};
 	class BBP_DOOR_BASE: BBP_WALL_BASE
 	{
+		class DamageSystem
+		{
+			class GlobalHealth
+			{
+				class Health
+				{
+					hitpoints=100;
+				};
+			};
+			class GlobalArmor
+			{
+				class Projectile
+				{
+					class Health
+					{
+						damage=0;
+					};
+					class Blood
+					{
+						damage=0;
+					};
+					class Shock
+					{
+						damage=0;
+					};
+				};
+				class FragGrenade
+				{
+					class Health
+					{
+						damage=0;
+					};
+					class Blood
+					{
+						damage=0;
+					};
+					class Shock
+					{
+						damage=0;
+					};
+				};
+			};
+			class DamageZones
+			{
+				class BBP_Tier_1
+				{
+					class Health
+					{
+						hitpoints=21000; //ONLY EDIT ME
+						transferToGlobalCoef=0;
+						healthLevels[]=
+						{
+							
+							{
+								1,
+								
+								{
+									"DZ\gear\camping\data\fence_pile_of_planks.rvmat"
+								}
+							},
+							
+							{
+								0.69999999,
+								
+								{
+									"DZ\gear\camping\data\fence_pile_of_planks_worn.rvmat"
+								}
+							},
+							
+							{
+								0.5,
+								
+								{
+									"DZ\gear\camping\data\fence_pile_of_planks_damage.rvmat"
+								}
+							},
+							
+							{
+								0.30000001,
+								
+								{
+									"DZ\gear\camping\data\fence_pile_of_planks_bdamage.rvmat"
+								}
+							},
+							
+							{
+								0,
+								
+								{
+									"DZ\gear\camping\data\fence_pile_of_planks_bdamage.rvmat"
+								}
+							}
+						};
+					};
+					class ArmorType
+					{
+						class Projectile
+						{
+							class Health
+							{
+								damage=1;
+							};
+							class Blood
+							{
+								damage=0;
+							};
+							class Shock
+							{
+								damage=0;
+							};
+						};
+						class Melee
+						{
+							class Health
+							{
+								damage=0.64999998;
+							};
+							class Blood
+							{
+								damage=0;
+							};
+							class Shock
+							{
+								damage=0;
+							};
+						};
+						class FragGrenade
+						{
+							class Health
+							{
+								damage=50;
+							};
+							class Blood
+							{
+								damage=0;
+							};
+							class Shock
+							{
+								damage=0;
+							};
+						};
+					};
+					componentNames[]=
+					{
+						"T1"
+					};
+					fatalInjuryCoef=-1;
+				};
+				class BBP_Tier_2
+				{
+					class Health
+					{
+						hitpoints=30000;//ONLY EDIT ME
+						transferToGlobalCoef=0;
+						healthLevels[]=
+						{
+							
+							{
+								1,
+								
+								{
+									"DZ\gear\camping\data\fence_pile_of_planks.rvmat"
+								}
+							},
+							
+							{
+								0.69999999,
+								
+								{
+									"DZ\gear\camping\data\fence_pile_of_planks_worn.rvmat"
+								}
+							},
+							
+							{
+								0.5,
+								
+								{
+									"DZ\gear\camping\data\fence_pile_of_planks_damage.rvmat"
+								}
+							},
+							
+							{
+								0.30000001,
+								
+								{
+									"DZ\gear\camping\data\fence_pile_of_planks_bdamage.rvmat"
+								}
+							},
+							
+							{
+								0,
+								
+								{
+									"DZ\gear\camping\data\fence_pile_of_planks_bdamage.rvmat"
+								}
+							}
+						};
+					};
+					class ArmorType
+					{
+						class Projectile
+						{
+							class Health
+							{
+								damage=1;
+							};
+							class Blood
+							{
+								damage=0;
+							};
+							class Shock
+							{
+								damage=0;
+							};
+						};
+						class Melee
+						{
+							class Health
+							{
+								damage=0.64999998;
+							};
+							class Blood
+							{
+								damage=0;
+							};
+							class Shock
+							{
+								damage=0;
+							};
+						};
+						class FragGrenade
+						{
+							class Health
+							{
+								damage=50;
+							};
+							class Blood
+							{
+								damage=0;
+							};
+							class Shock
+							{
+								damage=0;
+							};
+						};
+					};
+					componentNames[]=
+					{
+						"T2"
+					};
+					fatalInjuryCoef=-1;
+				};
+				class BBP_Tier_3
+				{
+					class Health
+					{
+						hitpoints=50000;//ONLY EDIT ME
+						transferToGlobalCoef=0;
+						healthLevels[]=
+						{
+							
+							{
+								1,
+								
+								{
+									"DZ\gear\camping\data\fence_pile_of_planks.rvmat"
+								}
+							},
+							
+							{
+								0.69999999,
+								
+								{
+									"DZ\gear\camping\data\fence_pile_of_planks_worn.rvmat"
+								}
+							},
+							
+							{
+								0.5,
+								
+								{
+									"DZ\gear\camping\data\fence_pile_of_planks_damage.rvmat"
+								}
+							},
+							
+							{
+								0.30000001,
+								
+								{
+									"DZ\gear\camping\data\fence_pile_of_planks_bdamage.rvmat"
+								}
+							},
+							
+							{
+								0,
+								
+								{
+									"DZ\gear\camping\data\fence_pile_of_planks_bdamage.rvmat"
+								}
+							}
+						};
+					};
+					class ArmorType
+					{
+						class Projectile
+						{
+							class Health
+							{
+								damage=1;
+							};
+							class Blood
+							{
+								damage=0;
+							};
+							class Shock
+							{
+								damage=0;
+							};
+						};
+						class Melee
+						{
+							class Health
+							{
+								damage=0.64999998;
+							};
+							class Blood
+							{
+								damage=0;
+							};
+							class Shock
+							{
+								damage=0;
+							};
+						};
+						class FragGrenade
+						{
+							class Health
+							{
+								damage=50;
+							};
+							class Blood
+							{
+								damage=0;
+							};
+							class Shock
+							{
+								damage=0;
+							};
+						};
+					};
+					componentNames[]=
+					{
+						"T3"
+					};
+					fatalInjuryCoef=-1;
+				};
+				//DOOR RAIDING
+				class t3_door: BBP_Tier_3
+				{
+					componentNames[]=
+					{
+						"t3_door"
+					};
+				};
+				class t2_door: BBP_Tier_2
+				{
+					componentNames[]=
+					{
+						"t2_door"
+					};
+				};
+				class t1_door: BBP_Tier_1
+				{
+					componentNames[]=
+					{
+						"t1_door"
+					};
+				};
+				class window_bars: BBP_Tier_1
+				{
+					componentNames[]=
+					{
+						"window_bars"
+					};
+				};
+				class Wall_Barbedwire_1: BBP_Tier_1
+				{
+					componentNames[]=
+					{
+						"wall_barbedwire_1",
+						"wall_barbedwire_1_mounted"
+					};
+					inventorySlots[]=
+					{
+						"Wall_Barbedwire_1"
+					};
+					fatalInjuryCoef=-1;
+				};
+				class Wall_Barbedwire_2: BBP_Tier_1
+				{
+					componentNames[]=
+					{
+						"wall_barbedwire_2",
+						"wall_barbedwire_2_mounted"
+					};
+					inventorySlots[]=
+					{
+						"Wall_Barbedwire_2"
+					};
+				};
+			};
+		};
 	};
 };
